35928b76304663bd44de71a752882db712ebee11
Donghyun Kim <crtmt97@gmail.com>
2023-02-09 13:16:19 +0900

    올바른괄호

--- 인프런JS알고리즘/sec6자료구조/올바른괄호.js
+++ 인프런JS알고리즘/sec6자료구조/올바른괄호.js
@@ -1,26 +1,8 @@
-let input = require('fs')
-  .readFileSync('input.txt')
-  .toString()
-  .trim()
-  .split('\n')
-  .map((a) => a.split(' ').map(Number));
-let [[num, budget], ...students] = input;
-let maximum = 0;
-for (let i = 0; i < num; i++) {
-  let remain = budget;
-  let count = 0;
-  let testArr = students
-    .map((a, index) => {
-      if (index === i) return a[0] / 2 + a[1];
-      return a[0] + a[1];
-    })
-    .sort((a, b) => a - b);
-  testArr.forEach((val) => {
-    if (remain > 0) {
-      remain -= val;
-      count++;
-    }
-  });
-  if (count >= maximum) maximum = count;
-}
-console.log(maximum);
+let input = require('fs').readFileSync('input.txt').toString().trim().split('');
+console.log(input);
+let stack = [];
+input.forEach((a) => {
+  a === '(' ? stack.push(a) : stack.pop();
+});
+if (stack.length === 0) console.log('YES');
+else console.log('NO');
