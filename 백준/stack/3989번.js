7d42d4c3c6e0214f95883337aef631fe754b1797
dhkim0306 <dhkim@krossgames.in>
2023-03-19 22:10:40 +0900

    실버4

---
+++ 백준/stack/3989번.js
@@ -0,0 +1,19 @@
+let input = require('fs')
+  .readFileSync('./input.txt')
+  .toString()
+  .trim()
+  .split('\n');
+const N = Number(input.shift());
+const isGoodStr = (str) => {
+  const stack = [];
+  const length = str.length;
+  for (let i = 0; i < length; i++) {
+    if (str[i] === stack[stack.length - 1]) stack.pop();
+    else stack.push(str[i]);
+  }
+  return stack.length === 0 ? 1 : 0;
+};
+
+const countGood = input.reduce((acc, str) => acc + isGoodStr(str), 0);
+
+console.log(countGood);
