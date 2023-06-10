d28ad376e8ba5d0bf872cba33a7f27201bbede48
Donghyun Kim <crtmt97@gmail.com>
2023-06-11 00:31:32 +0900

    [Bronze II] Title: Hashing, Time: 148 ms, Memory: 9332 KB -BaekjoonHub

---
+++ 백준/Bronze/15829. Hashing/Hashing.js
@@ -0,0 +1,15 @@
+const fs = require('fs');
+const filePath = process.platform === 'linux' ? '/dev/stdin' : 'input.txt';
+let input = require('fs').readFileSync(filePath).toString().trim().split('\n');
+const N = input.shift();
+const strArr = input.pop().split('');
+
+const formatAlphabet = (alphabet, index) => {
+  return (alphabet.charCodeAt(0) - 96) * 31 ** index;
+};
+const result = strArr.reduce(
+  (sum, alphabet, index) => sum + formatAlphabet(alphabet, index),
+  0
+);
+
+console.log(result);
