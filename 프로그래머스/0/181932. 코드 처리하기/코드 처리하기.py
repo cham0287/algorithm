699b9e8ccab19993fa79b7c32cd2e14b98906ba1
Donghyun Kim <crtmt97@gmail.com>
2024-09-16 18:01:26 +0900

    [level 0] Title: 코드 처리하기, Time: 12.41 ms, Memory: 10.3 MB -BaekjoonHub

---
+++ 프로그래머스/0/181932. 코드 처리하기/코드 처리하기.py
@@ -0,0 +1,17 @@
+def solution(code):
+    answer = ''
+    
+    mode = 0
+    for i in range(0,len(code)):
+        if code[i] == '1':
+            if mode==0:
+                mode=1
+            else: mode=0
+        else:
+            if mode==0:
+                if i%2==0:
+                    answer += code[i]
+            else:
+                if i%2==1:
+                      answer += code[i]
+    return answer if answer else 'EMPTY'
