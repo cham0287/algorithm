de5dee90c03d29a4f872020c10b3aca1e3896880
Donghyun Kim <crtmt97@gmail.com>
2025-03-16 15:26:59 +0900

    Time: 5 ms (98.28%), Space: 69.6 MB (7.91%) - LeetHub

--- 0217-contains-duplicate/0217-contains-duplicate.js
+++ 0217-contains-duplicate/0217-contains-duplicate.js
@@ -3,17 +3,5 @@
  * @return {boolean}
  */
 var containsDuplicate = function(nums) {
-    let answer = false;
-    const history ={}
-
-    for (let i=0; i<nums.length; i++){
-        if(history[nums[i]]){
-            answer = true;
-        } else {
-            history[nums[i]] = true
-        }
-    }
-
-    return answer
-    
+   return [...new Set(nums)].length !== nums.length;
 };
